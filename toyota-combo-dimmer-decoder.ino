ec9de6fbf932107b6ca60e554a91b877e45ae32b
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-06-25 22:43:25 +1000

    Update toyota-combo-dimmer-decoder.ino

--- toyota-combo-dimmer-decoder.ino
+++ toyota-combo-dimmer-decoder.ino
@@ -1,43 +1,89 @@
+// demo: CAN-BUS Shield, receive data with interrupt mode, and set mask and filter
+//
+// when in interrupt mode, the data coming can't be too fast, must >20ms, or else you can use check mode
+// loovee, 2014-7-8
+
 #include <SPI.h>
-#include "mcp2515_can.h"
- 
-/*SAMD core*/
-#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-    #define SERIAL SerialUSB
+
+#define CAN_2515
+// #define CAN_2518FD
+
+// Set SPI CS Pin according to your hardware
+
+#if defined(SEEED_WIO_TERMINAL) && defined(CAN_2518FD)
+// For Wio Terminal w/ MCP2518FD RPi Hat：
+// Channel 0 SPI_CS Pin: BCM 8
+// Channel 1 SPI_CS Pin: BCM 7
+// Interupt Pin: BCM25
+const int SPI_CS_PIN  = BCM8;
+const int CAN_INT_PIN = BCM25;
 #else
-    #define SERIAL Serial
+
+// For Arduino MCP2515 Hat:
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 10;
+const int CAN_INT_PIN = 2;
 #endif
- 
-const int SPI_CS_PIN = 9;
+
+
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+
+#ifdef CAN_2515
+#include "mcp2515_can.h"
 mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
- 
+#endif
+
+unsigned char flagRecv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
+
 void setup() {
-    SERIAL.begin(115200);
-    while(!Serial){};
- 
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
-        SERIAL.println("CAN BUS Shield init fail");
-        SERIAL.println(" Init CAN BUS Shield again");
-        delay(100);
-    }
-    SERIAL.println("CAN BUS Shield init ok!");
+  SERIAL_PORT_MONITOR.begin(115200);
+  while (!Serial) {};
+  attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), MCP2515_ISR, FALLING); // start interrupt
+  while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+    SERIAL_PORT_MONITOR.println("CAN init fail, retry...");
+    delay(100);
+  }
+  SERIAL_PORT_MONITOR.println("CAN init ok!");
+
+  /*
+      set mask, set both the mask to 0x3ff
+  */
+  CAN.init_Mask(0, 0, 0x3ff);                         // there are 2 mask in mcp2515, you need to set both of them
+  CAN.init_Mask(1, 0, 0x3ff);
+
+
+  /*
+      set filter, we can receive id from 0x04 ~ 0x09
+  */
+  CAN.init_Filt(0, 0, 0x620);                          // there are 6 filter in mcp2515
 }
- 
-unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+void MCP2515_ISR() {
+  flagRecv = 1;
+}
+
 void loop() {
-    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
-    stmp[7] = stmp[7] + 1;
-    if (stmp[7] == 100) {
-        stmp[7] = 0;
-        stmp[6] = stmp[6] + 1;
- 
-        if (stmp[6] == 100) {
-            stmp[6] = 0;
-            stmp[5] = stmp[6] + 1;
-        }
-    }
- 
-    CAN.sendMsgBuf(0x00, 0, 8, stmp);
-    delay(100);                       // send data per 100ms
-    SERIAL.println("CAN BUS sendMsgBuf ok!");
+  flagRecv = 0;                // clear flag
+  CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+
+  SERIAL_PORT_MONITOR.println("\r\n------------------------------------------------------------------");
+  SERIAL_PORT_MONITOR.print("Get Data From id: ");
+  SERIAL_PORT_MONITOR.println(CAN.getCanId());
+  for (int i = 0; i < len; i++) { // print the data
+    SERIAL_PORT_MONITOR.print("0x");
+    SERIAL_PORT_MONITOR.print(buf[i], HEX);
+    SERIAL_PORT_MONITOR.print("\t");
+    SERIAL_PORT_MONITOR.println();
+  }
 }
+
+/*********************************************************************************************************
+    END FILE
+*********************************************************************************************************/

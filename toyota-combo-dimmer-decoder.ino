e63d8da0f202a1ebe3841daebf35546d0b2510ad
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-06-26 12:18:13 +1000

    Update toyota-combo-dimmer-decoder.ino

--- toyota-combo-dimmer-decoder.ino
+++ toyota-combo-dimmer-decoder.ino
@@ -1,41 +1,10 @@
-// demo: CAN-BUS Shield, receive data with interrupt mode, and set mask and filter
-//
-// when in interrupt mode, the data coming can't be too fast, must >20ms, or else you can use check mode
-// loovee, 2014-7-8
-
 #include <SPI.h>
+#include "mcp2515_can.h"
 
-#define CAN_2515
-// #define CAN_2518FD
-
-// Set SPI CS Pin according to your hardware
-
-#if defined(SEEED_WIO_TERMINAL) && defined(CAN_2518FD)
-// For Wio Terminal w/ MCP2518FD RPi Hat：
-// Channel 0 SPI_CS Pin: BCM 8
-// Channel 1 SPI_CS Pin: BCM 7
-// Interupt Pin: BCM25
-const int SPI_CS_PIN  = BCM8;
-const int CAN_INT_PIN = BCM25;
-#else
-
-// For Arduino MCP2515 Hat:
-// the cs pin of the version after v1.1 is default to D9
-// v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 10;
 const int CAN_INT_PIN = 2;
-#endif
 
-
-#ifdef CAN_2518FD
-#include "mcp2518fd_can.h"
-mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
-#endif
-
-#ifdef CAN_2515
-#include "mcp2515_can.h"
-mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
-#endif
+mcp2515_can CAN(SPI_CS_PIN);
 
 unsigned char flagRecv = 0;
 unsigned char len = 0;
@@ -52,16 +21,9 @@ void setup() {
   }
   SERIAL_PORT_MONITOR.println("CAN init ok!");
 
-  /*
-      set mask, set both the mask to 0x3ff
-  */
   CAN.init_Mask(0, 0, 0x3ff);                         // there are 2 mask in mcp2515, you need to set both of them
   CAN.init_Mask(1, 0, 0x3ff);
 
-
-  /*
-      set filter, we can receive id from 0x04 ~ 0x09
-  */
   CAN.init_Filt(0, 0, 0x620);                          // there are 6 filter in mcp2515
 }
 
@@ -70,20 +32,16 @@ void MCP2515_ISR() {
 }
 
 void loop() {
-  flagRecv = 0;                // clear flag
-  CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+  flagRecv = 0;
+  CAN.readMsgBuf(&len, buf);
 
-  SERIAL_PORT_MONITOR.println("\r\n------------------------------------------------------------------");
-  SERIAL_PORT_MONITOR.print("Get Data From id: ");
-  SERIAL_PORT_MONITOR.println(CAN.getCanId());
-  for (int i = 0; i < len; i++) { // print the data
-    SERIAL_PORT_MONITOR.print("0x");
-    SERIAL_PORT_MONITOR.print(buf[i], HEX);
-    SERIAL_PORT_MONITOR.print("\t");
-    SERIAL_PORT_MONITOR.println();
+  if (CAN.getCanId() == 1568) {
+   for (int i = 0; i < len; i++) {
+	 if (buf[4] == 240) {
+		 Serial.println("dimmed");
+	 } else if (buf[4] == 208) {
+		 Serial.println("bright");
+	 }
+   }
   }
 }
-
-/*********************************************************************************************************
-    END FILE
-*********************************************************************************************************/

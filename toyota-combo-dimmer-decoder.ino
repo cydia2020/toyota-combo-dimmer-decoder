f89d8ff5c74cbda9177f901af86ba0144ceb03b6
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-10-02 23:51:36 +1000

    TEST

--- toyota-combo-dimmer-decoder.ino
+++ toyota-combo-dimmer-decoder.ino
@@ -12,12 +12,13 @@ unsigned char buf[8];
 char str[20];
 
 void setup() {
+  delay(2000);
   pinMode(A1, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), MCP2515_ISR, FALLING); // start interrupt
   CAN.begin(CAN_500KBPS);
 
-  CAN.init_Mask(0, 0, 0x3ff);                         // there are 2 mask in mcp2515, you need to set both of them
-  CAN.init_Mask(1, 0, 0x3ff);
+  CAN.init_Mask(0, 0, 0x7FF);                         // there are 2 mask in mcp2515, you need to set both of them
+  CAN.init_Mask(1, 0, 0x7FF);
 
   CAN.init_Filt(0, 0, 0x620);                          // there are 6 filter in mcp2515
 }

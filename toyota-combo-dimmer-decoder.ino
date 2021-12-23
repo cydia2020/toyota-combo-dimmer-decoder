8655222cfddbbb60f73bf2b4696b9d3d12556651
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-12-23 23:33:41 +1100

    cleanup

--- toyota-combo-dimmer-decoder.ino
+++ toyota-combo-dimmer-decoder.ino
@@ -1,24 +1,21 @@
 #include <SPI.h>
 #include "mcp2515_can.h"
 
-const int SPI_CS_PIN = 10;
-const int CAN_INT_PIN = 2;
+const int SPI_CS_PIN = 17;
 
 mcp2515_can CAN(SPI_CS_PIN);
 
-unsigned char flagRecv = 0;
-unsigned char len = 0;
-unsigned char buf[8];
-char str[20];
+bool flagRecv = 0;
+char len = 0;
+char buf[8];
+unsigned long canId = 0;
 
 void setup() {
-  sleep(2000);
-  pinMode(A1, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), MCP2515_ISR, FALLING); // start interrupt
+  pinMode(A0, OUTPUT);
   CAN.begin(CAN_500KBPS);
 
-  CAN.init_Mask(0, 0, 0x7FF);                         // there are 2 mask in mcp2515, you need to set both of them
-  CAN.init_Mask(1, 0, 0x7FF);
+  CAN.init_Mask(0, 0, 0x3FF);                         // there are 2 mask in mcp2515, you need to set both of them
+  CAN.init_Mask(1, 0, 0x3FF);
 
   CAN.init_Filt(0, 0, 0x620);                          // there are 6 filter in mcp2515
 }
@@ -30,10 +27,13 @@ void MCP2515_ISR() {
 void loop() {
   flagRecv = 0;
   CAN.readMsgBuf(&len, buf);
+  canId = CAN.getCanId();
 
-  if (bitRead(buf[4], 6) == 1) {
-    digitalWrite(A1, HIGH);
-  } else if (bitRead(buf[4], 6) == 0) {
-    digitalWrite(A1, LOW);
+  if ((canId = 1568) && (bitRead(buf[4], 6) == 1)) {
+    digitalWrite(A0, HIGH);
+  } else if ((canId = 1568) && (bitRead(buf[4], 6) == 0)) {
+    digitalWrite(A0, LOW);
   }
+
+  delay(100);
 }
